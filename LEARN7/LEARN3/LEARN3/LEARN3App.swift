941fbc830f5df23b47852444d4400e9f28743aa4
rennn2002 <n11ren21@icloud.com>
2021-02-10 22:04:53 +0900

    Initial Commit

---
+++ LEARN7/LEARN3/LEARN3/LEARN3App.swift
@@ -0,0 +1,17 @@
+//
+//  LEARN3App.swift
+//  LEARN3
+//
+//  Created by Nomura Rentaro on 2021/02/10.
+//
+
+import SwiftUI
+
+@main
+struct LEARN3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
