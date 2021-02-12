3e65a89daae827206e8f9dcca47fd982be0ba593
rennn2002 <n11ren21@icloud.com>
2021-02-12 16:58:56 +0900

    Initial Commit

---
+++ LEARN4/LEARN4/LEARN4App.swift
@@ -0,0 +1,17 @@
+//
+//  LEARN4App.swift
+//  LEARN4
+//
+//  Created by Nomura Rentaro on 2021/02/12.
+//
+
+import SwiftUI
+
+@main
+struct LEARN4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
