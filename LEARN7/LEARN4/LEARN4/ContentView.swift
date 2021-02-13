d9fdaa796432be1ddde4ca4396fe96a19c160659
rennn2002 <n11ren21@icloud.com>
2021-02-13 21:03:38 +0900

    13, Feb 2021

--- LEARN7/LEARN4/LEARN4/ContentView.swift
+++ LEARN7/LEARN4/LEARN4/ContentView.swift
@@ -7,10 +7,47 @@
 
 import SwiftUI
 
+struct ColorView: View, Identifiable {
+    @State var id = UUID()
+    @State var item:(Text, Text, Color)
+    let width = UIScreen.main.bounds.size.width - 10
+    var body: some View {
+        TupleView(
+            (
+                item.0
+                    .frame(width: width, alignment: .leading)
+                    .font(.largeTitle)
+                    .foregroundColor(item.2),
+                
+                item.1
+                    .frame(width:width, alignment: .leading)
+                    .font(.headline)
+                    .foregroundColor(item.2),
+                
+                Divider()
+            )
+        )
+    }
+}
+
 struct ContentView: View {
+    let data = [
+        ColorView(item:(Text("One"), Text("Oh! First Item!"), Color.red)),
+        ColorView(item:(Text("Two"), Text("This is second item!"), Color.green)),
+        ColorView(item:(Text("Three"), Text("It's last item!"), Color.blue)),
+    ]
     
     var body: some View {
-        Text("Hello, world");
+        Text("Tupple View")
+            .font(.largeTitle)
+        
+        Divider()
+        
+        ForEach(data) {item in
+            item
+        }
+        
+        Spacer(minLength: 0)
     }
 }
 
