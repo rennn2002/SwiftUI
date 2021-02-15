f01fc5fc41a2e66b52c1150c49211f5289ba8ff1
rennn2002 <n11ren21@icloud.com>
2021-02-15 13:06:58 +0900

    Initial Commit

---
+++ LEARN7/LEARN5/LEARN5/LEARN5App.swift
@@ -0,0 +1,17 @@
+//
+//  LEARN5App.swift
+//  LEARN5
+//
+//  Created by Nomura Rentaro on 2021/02/15.
+//
+
+import SwiftUI
+
+@main
+struct LEARN5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
