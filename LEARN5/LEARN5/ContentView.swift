d48da8fdd950ea009a688f3c88359c8db29a99e5
rennn2002 <n11ren21@icloud.com>
2021-02-15 13:32:08 +0900

    15, Feb 2021

--- LEARN5/LEARN5/ContentView.swift
+++ LEARN5/LEARN5/ContentView.swift
@@ -7,15 +7,61 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct DatabaseView: View, Identifiable {
+    
+    let id = UUID()
+    let userName: String
+    let message: String
+    let color: Color
+    let age: Int
+    let status: Bool
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Text(self.userName)
+                .font(.largeTitle)
+            Text(self.message)
+                .font(.headline)
+            
+            Text(String(self.age))
+            
+            Text("status: \(self.status ? "No":"Yes")")
+            
+        }.background(self.color)
+    }
+    
+}
+
+struct DetailView: View{
+    
+    var Database: DatabaseView
+    var body: some View {
+        Text("Detail")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+struct ContentView: View {
+    
+    var myData = [
+        DatabaseView(userName:"Ren", message:"hello!", color:Color.red, age:18, status: true),
+        DatabaseView(userName:"Keigo", message:"hi!", color:Color.yellow, age:15, status: false)
+    ]
+    var body : some View {
+        VStack {
+            List(myData) {item in
+                item
+            }
+        }
+    }
+    
+}
+
+struct Content_Previews: PreviewProvider {
+    
+    static var previews:some View {
         ContentView()
+        
     }
 }
+
