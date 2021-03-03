d967ae5337e1213dafbee2368747f2fbb02890e2
rennn2002 <n11ren21@icloud.com>
2021-03-03 14:37:47 +0900

    Initial Commit

---
+++ LEARN9/LEARN9/LEARN9App.swift
@@ -0,0 +1,17 @@
+//
+//  LEARN9App.swift
+//  LEARN9
+//
+//  Created by Nomura Rentaro on 2021/03/03.
+//
+
+import SwiftUI
+
+@main
+struct LEARN9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
