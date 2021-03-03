5fca92e0f2b45c0b62a5eede7f699143925fdf2d
rennn2002 <n11ren21@icloud.com>
2021-03-03 16:08:12 +0900

    Mar 3, 2021

--- LEARN9/LEARN9/ContentView.swift
+++ LEARN9/LEARN9/ContentView.swift
@@ -9,8 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+           
+            HStack {
+                Image("1")
+                    .resizable()
+                    .frame(width:180, height:180)
+                    .clipShape(Circle())
+                    .shadow(color:Color.white, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x:10, y: 10)
+                    .overlay(Circle().stroke(Color.white, lineWidth:3))
+                    .position(x:100, y:100)
+                
+                Image("2")
+                    .resizable()
+                    .frame(width:180, height:180)
+                    .clipShape(Circle())
+                    .position(x:100, y:100)
+            }
+            HStack {
+                Image("3")
+                    .resizable()
+                    .frame(width:180, height:180)
+                    .clipShape(Circle())
+                    .position(x:100, y:30)
+                
+                Image("4")
+                    .resizable()
+                    .frame(width:180, height:180)
+                    .clipShape(Circle())
+                    .position(x:100, y:30)
+            }
+            HStack {
+                Image("5")
+                    .resizable()
+                    .frame(width:180, height:180)
+                    .clipShape(Circle())
+                    .position(x:100, y:-40)
+                
+                
+            }
+        }
     }
 }
 
