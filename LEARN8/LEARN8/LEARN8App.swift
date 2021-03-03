a1eec8dab352cd90d069e1c463a8b17736277d1f
rennn2002 <n11ren21@icloud.com>
2021-03-03 10:14:07 +0900

    Initial Commit

---
+++ LEARN8/LEARN8/LEARN8App.swift
@@ -0,0 +1,17 @@
+//
+//  LEARN8App.swift
+//  LEARN8
+//
+//  Created by Nomura Rentaro on 2021/03/03.
+//
+
+import SwiftUI
+
+@main
+struct LEARN8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
