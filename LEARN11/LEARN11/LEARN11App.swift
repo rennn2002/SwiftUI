821131097f1a95682b50ab3c02c78ae0ab6c662c
ren_nomura <47625679+rennn2002@users.noreply.github.com>
2021-03-04 21:22:02 +0900

    Initial Commit

---
+++ LEARN11/LEARN11/LEARN11App.swift
@@ -0,0 +1,17 @@
+//
+//  LEARN11App.swift
+//  LEARN11
+//
+//  Created by Nomura Rentaro on 2021/03/04.
+//
+
+import SwiftUI
+
+@main
+struct LEARN11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
