90e7e9b6625d2d9e6eb6f2135f60b4fcce40b65e
ren_nomura <47625679+rennn2002@users.noreply.github.com>
2021-03-05 21:22:42 +0900

    Initial Commit

---
+++ LEARN12/LEARN12/LEARN12App.swift
@@ -0,0 +1,17 @@
+//
+//  LEARN12App.swift
+//  LEARN12
+//
+//  Created by Nomura Rentaro on 2021/03/05.
+//
+
+import SwiftUI
+
+@main
+struct LEARN12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
