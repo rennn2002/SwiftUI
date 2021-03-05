3b6b12f4da2c92f1ca58f4955561ab49b7f1f80f
ren_nomura <47625679+rennn2002@users.noreply.github.com>
2021-03-05 17:23:46 +0900

    Initial Commit

---
+++ LEARN11/LEARN11/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LEARN11
+//
+//  Created by Nomura Rentaro on 2021/03/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
