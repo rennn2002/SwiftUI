c0bcaf90da13659246a123bd9fae86d8b60cdd55
ren_nomura <47625679+rennn2002@users.noreply.github.com>
2021-03-11 22:41:55 +0900

    Initial Commit

---
+++ LEARN14/LEARN14/LEARN14App.swift
@@ -0,0 +1,17 @@
+//
+//  LEARN14App.swift
+//  LEARN14
+//
+//  Created by Nomura Rentaro on 2021/03/11.
+//
+
+import SwiftUI
+
+@main
+struct LEARN14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
