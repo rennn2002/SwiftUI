bb82e1534cdb3fc9e14dbf735c1ddb2711eff283
ren_nomura <47625679+rennn2002@users.noreply.github.com>
2021-03-15 00:07:55 +0900

    Mar 15, 2021

--- LEARN15/LEARN15/ContentView.swift
+++ LEARN15/LEARN15/ContentView.swift
@@ -6,15 +6,58 @@
 //
 
 import SwiftUI
+import Foundation
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+class LoremService {
+    let url:URL = URL(string:"https://loripsum.net/api/plaintext")!
+    
+    func getText(res: @escaping(String)->()) {
+        URLSession.shared.dataTask(with: url){ data, response, error in
+            
+            DispatchQueue.main.async {
+                res(String(data: data!, encoding: String.Encoding.utf8)!)
+            }
+        }.resume()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+class LoremModel: ObservableObject {
+    @Published var data = String()
+    
+    init() {
+        self.getData()
+    }
+    
+    func getData() {
+        LoremService().getText { data in
+            self.data = data
+        }
+    }
+}
+
+struct ContentView: View {
+    @State var msg = "plain text."
+    @ObservedObject var loremmodel = LoremModel()
+    
+    var body: some View {
+        VStack {
+            Text(msg)
+                .font(.title)
+            
+            Divider()
+            
+            Text(loremmodel.data)
+                .font(.body)
+                .padding(10)
+            
+            Divider()
+            
+            Spacer(minLength: 0)
+        }
+    }
+}
+
+struct ContetnView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
