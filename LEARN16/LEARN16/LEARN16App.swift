275f7145ca16d51d4f1e35ba91bf3bf8c210d5d0
ren_nomura <47625679+rennn2002@users.noreply.github.com>
2021-03-19 10:23:02 +0900

    Initial Commit

---
+++ LEARN16/LEARN16/LEARN16App.swift
@@ -0,0 +1,17 @@
+//
+//  LEARN16App.swift
+//  LEARN16
+//
+//  Created by Nomura Rentaro on 2021/03/19.
+//
+
+import SwiftUI
+
+@main
+struct LEARN16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
