3d5ffec7d040e3b0a13997ed732fe149570f853a
ren_nomura <47625679+rennn2002@users.noreply.github.com>
2021-03-20 22:26:55 +0900

    Initial Commit

---
+++ LEARN17/LEARN17/LEARN17App.swift
@@ -0,0 +1,17 @@
+//
+//  LEARN17App.swift
+//  LEARN17
+//
+//  Created by Nomura Rentaro on 2021/03/20.
+//
+
+import SwiftUI
+
+@main
+struct LEARN17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
