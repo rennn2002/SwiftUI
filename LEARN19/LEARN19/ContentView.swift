bde258f5f145f339414d58f0d72e5436afca2849
ren_nomura <47625679+rennn2002@users.noreply.github.com>
2021-03-21 12:22:07 +0900

    Mar 21, 2021

--- LEARN19/LEARN19/ContentView.swift
+++ LEARN19/LEARN19/ContentView.swift
@@ -7,10 +7,85 @@
 
 import SwiftUI
 
+class TextFileService {
+    let fname: String
+    let path:URL
+    
+    init(filename:String) {
+        self.fname = filename
+        let dir = FileManager.default.urls(
+            for: .documentDirectory,
+            in: .userDomainMask
+        ).first
+        path = dir!.appendingPathComponent(fname)
+    }
+    
+    func getText()->String {
+        var result = "no data"
+        do {
+            result = try String(contentsOf: path, encoding: String.Encoding.utf8)
+        } catch {
+            result = "can't get data"
+        }
+        return result
+    }
+    
+    func setText(data:String)->Void {
+        try! data.write(
+            to: path, atomically: false,
+            encoding: String.Encoding.utf8
+        )
+    }
+}
+
+class TextFileModel: ObservableObject {
+    @Published var data: String
+    let service:TextFileService
+    
+    init(filename: String) {
+        data = String()
+        service = TextFileService(filename: filename)
+        self.load()
+    }
+    
+    func load() {
+        self.data = service.getText()
+    }
+    
+    func save() {
+        service.setText(data: self.data)
+    }
+}
+
 struct ContentView: View {
+    @State var msg: String = "Plain text"
+    @ObservedObject var textmodel = TextFileModel(filename:"sample.txt")
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Text(self.msg)
+                .font(.title)
+            
+            Divider()
+            
+            TextField("load data", text: $textmodel.data)
+                .font(.body)
+                .padding(10)
+            
+            Divider()
+            
+            Button("save data", action: {
+                self.textmodel.save()
+                self.msg = "saved..."
+            }).font(.headline)
+            .frame(width:300, height:50)
+            .border(Color.blue)
+            
+            Divider()
+            
+            Spacer(minLength: 0)
+        }
     }
 }
 
