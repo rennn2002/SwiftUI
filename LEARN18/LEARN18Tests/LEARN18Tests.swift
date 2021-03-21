353c61a78e75512c4bbe032b7df677d8fa958958
ren_nomura <47625679+rennn2002@users.noreply.github.com>
2021-03-21 11:12:30 +0900

    Initial Commit

---
+++ LEARN18/LEARN18Tests/LEARN18Tests.swift
@@ -0,0 +1,33 @@
+//
+//  LEARN18Tests.swift
+//  LEARN18Tests
+//
+//  Created by Nomura Rentaro on 2021/03/21.
+//
+
+import XCTest
+@testable import LEARN18
+
+class LEARN18Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
